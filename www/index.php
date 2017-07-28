99ec245a09c3802a68fe2c2c7951e19d3e56cfb2
Andrei Timotin <andreitimotin11@gmail.com>
2017-07-28 22:22:34 +0300

    test

--- www/index.php
+++ www/index.php
@@ -5,3 +5,25 @@
  * Date: 28.07.2017
  * Time: 21:05
  */
+?>
+<html>
+<head>
+	<title>OOP</title>
+</head>
+<body>
+<?php
+
+class SomeClass
+{
+	public function qer($x){
+	}
+}
+$obj = new SomeClass();
+
+var_dump(
+	$obj instanceof stdClass
+)
+
+?>
+</body>
+</html>
